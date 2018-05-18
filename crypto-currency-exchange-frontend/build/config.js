d2eed3c6ca208ebc79b77305c98651d71ae9e21c
Dmitrii Surmin <surmin2005@yandex.ru>
2018-05-19 02:56:31 +0300

    add test endpoint, add fake api

--- crypto-currency-exchange-frontend/build/config.js
+++ crypto-currency-exchange-frontend/build/config.js
@@ -1,5 +1,5 @@
 (function() {
   var global = window;
-  var href = 'http://localhost:3000' || global.document.location.origin
+  var href = 'http://localhost:3004' || global.document.location.origin
   global.endpointOrigin = href
 }())
--- crypto-currency-exchange-frontend/src/Pages/SellerBuyer/TestEndpoint.js
+++ crypto-currency-exchange-frontend/src/Pages/SellerBuyer/TestEndpoint.js
@@ -1,13 +1,58 @@
 import React, { Component } from 'react'
-import { Input, Form } from 'antd'
+import { Input, Form, Button } from 'antd'
 
 class TestEndpoint extends Component {
+  state = {
+    token: '',
+    phone: '',
+    wallet: ''
+  }
+
+  onChange = (e) => {
+    e.preventDefault()
+    const { id, value } = e.target
+    this.setState({ [id]: value })
+  }
+
+  submit = () => {
+    fetch(`${window.endpointOrigin}/createaccountqiwi`, {
+      method: 'POST',
+      body: JSON.stringify(this.state)
+    })
+      .then(res => res.json())
+      .then(console.log)
+  }
+
   render() {
+    const { token, phone, wallet } = this.state
+
     return (
       <Form>
         <Form.Item>
-          <Input />
+          <Input
+            id='token'
+            addonBefore='token'
+            onChange={this.onChange}
+            value={token}
+          />
         </Form.Item>
+        <Form.Item>
+          <Input
+            id='phone'
+            addonBefore='phone'
+            onChange={this.onChange}
+            value={phone}
+          />
+        </Form.Item>
+        <Form.Item>
+          <Input
+            id='wallet'
+            addonBefore='wallet'
+            onChange={this.onChange}
+            value={wallet}
+          />
+        </Form.Item>
+        <Button onClick={this.submit}>Request</Button>
       </Form>
     )
   }
