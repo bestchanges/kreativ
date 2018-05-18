149cfaa4db3e28ea562714eed5bbd095b5211cdf
kostik <kostyavelikiyi@mai.ru>
2018-05-19 01:34:54 +0300

    added get balance qiwi

---
+++ server/get_qiwi_balance.py
@@ -0,0 +1,29 @@
+import requests
+
+def get_qiwi_balance(token, phone):
+    api_url = 'https://edge.qiwi.com/funding-sources/v2/persons/' + phone + '/accounts'
+
+    headers = {'Content-Type': 'application/json',
+               'Accept': 'application/json',
+               'Authorization': 'Bearer ' + token}
+
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        for c in data['accounts']:
+            if c['hasBalance']:
+                return c['balance']['amount']
+            else:
+                print('no balance')
+                return None
+    else:
+        print('[!] HTTP {0} calling [{1}]'.format(response.status_code, api_url))
+        return None
+
+
+tkn = '7820a390d136f825461739c26ae7324b'
+
+phn = '79636853224'
+
+print(get_qiwi_balance(tkn, phn))
