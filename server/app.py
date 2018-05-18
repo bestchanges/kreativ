bdf656af5ec3cd88655db5bf33de04aea7e325a7
Egor Fedorov <egor.fedorov@gmail.com>
2018-05-19 02:00:46 +0300

    sampel

--- server/app.py
+++ server/app.py
@@ -1,5 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 mongo = PyMongo(app)
+
+@app.route('/', )
+def hello_world():
+    json_data = request.get_json()
+    name = json_data['name']
+    data = {
+        'name' : name,
+    }
+    account = mongo.db.account.add(data)
+    return jsonify({'result': "OK"})
