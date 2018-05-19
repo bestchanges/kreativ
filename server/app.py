32fe6a3a89cbd605be32015be59074471523ea88
Egor Fedorov <egor.fedorov@gmail.com>
2018-05-19 04:26:42 +0300

    asd

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 mongo = PyMongo(app)
 
 @app.route('/', )
