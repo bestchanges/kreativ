ddd87cc9a84e59c483ece64becfa8ad03d24dbb2
Egor Fedorov <egor.fedorov@gmail.com>
2018-05-20 06:14:21 +0300

    add frontend

--- server/app.py
+++ server/app.py
@@ -2,14 +2,11 @@ from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 
-app = Flask(__name__)
+#app = Flask(__name__)
+app = Flask(__name__, static_folder="../crypto-currency-exchange-frontend/build", static_url_path="")
 app.secret_key = "super secret ke!"
 CORS(app)
 mongo = PyMongo(app)
-
-@app.route('/', )
-def hello_world():
-    return jsonify({'result': "OK"})
 
 import view
 import tr
